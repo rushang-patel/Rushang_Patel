90d7d6d435be0a0e1a3a62591c792032414af6d8
Rushang Patel <rushang1993@gmail.com>
2023-08-29 09:53:07 -0500

    about me update

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -4,7 +4,13 @@ const AboutMe = () => {
   return (
     <div>
       <h1>About Me</h1>
-      <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime reiciendis culpa ad voluptas voluptatum dicta earum! Corrupti inventore nesciunt perferendis illum, quasi rem, quia minima eaque quam sunt voluptatem neque.</h4>
+      <h4>Hi, I'm <h2>Rushang</h2>, a software engineer with a unique background in mechanical engineering, real estate, and hospitality. I am passionate about using technology to solve real-world problems.
+
+My mechanical engineering degree gave me a strong foundation in problem-solving and systems thinking. This has helped me to be successful in software engineering, where I am constantly faced with new challenges.
+
+My experience in real estate and hospitality has given me a deep understanding of how businesses operate. This has helped me to develop software solutions that are practical and user-friendly.
+
+I am a hard worker and I am always willing to go the extra mile. I am also a team player and I am always willing to help others.</h4>
       {}
     </div>
   );
--- src/components/pages/Contacts.jsx
+++ src/components/pages/Contacts.jsx
@@ -4,6 +4,7 @@ const Contacts = () => {
   return (
     <div>
       <h1>Contacts</h1>
+      <h4>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo laboriosam animi nostrum, autem facilis esse ea veniam aut tempora, rerum, aperiam velit repellendus voluptates illum odit sed. Fuga, magnam dicta?</h4>
       {}
     </div>
   );
